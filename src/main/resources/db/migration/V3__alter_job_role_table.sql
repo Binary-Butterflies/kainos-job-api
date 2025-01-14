2d38bed48ae843a9194711ff22adab60df13ab1d
dylanmckk <dylan.mckenzie@kainos.com>
2025-01-14 11:38:12 +0000

    Removed default empty values

--- src/main/resources/db/migration/V3__alter_job_role_table.sql
+++ src/main/resources/db/migration/V3__alter_job_role_table.sql
@@ -14,14 +14,5 @@ ADD sharepointUrl VARCHAR(512) NOT NULL,
 ADD statusId INT NOT NULL,
 ADD numberOfOpenPositions INT NOT NULL;
 
--- Disable safe updates temporarily in order to set fields for existing jobs
-SET SQL_SAFE_UPDATES = 0;
-UPDATE `job_roles` SET description = "";
-UPDATE `job_roles` SET responsibilities = "";
-UPDATE `job_roles` SET sharepointUrl = "";
-UPDATE `job_roles` SET statusId = 1;
-UPDATE `job_roles` SET numberOfOpenPositions = 1;
-SET SQL_SAFE_UPDATES = 1;
-
 ALTER TABLE job_roles
 ADD CONSTRAINT fk_statusId FOREIGN KEY (statusId) REFERENCES status(statusId);
